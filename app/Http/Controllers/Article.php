b4b624128713519e4ac0843845c17f9eea001f6f
Enzhi98 <55082647+Enzhi98@users.noreply.github.com>
2022-08-22 18:53:04 +0400

    Update Article.php

--- app/Http/Controllers/Article.php
+++ app/Http/Controllers/Article.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class Article extends Controller
 {
-    //
+    // Добавить в родительский контроллер методы для хранения данных, которые будет возвращать массивы.
+    //Массивы должны содержать информацию о категориях новостей(мимнимум 5), и новостях (минимум 4 для каждой категории)
+    public function category(){
+        $categories=$this->getCategory();
+        return view('news.category', [
+            'ctgList'=> $categories
+        ]);
+    }
+    public function index() {
+        $news=$this->getNews();
+        return view('news.index',[
+            'newsList'=> $news
+        ]);
+    }
+    public function show(int $id) {
+        $news = $this->getNews($id);
+        return view('news.show', [
+            'news'=> $news
+        ]);
+    }
 }
