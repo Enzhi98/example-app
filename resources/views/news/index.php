79ecf8ab6a3b459b4a31162ca8ef4949ab9102e0
Enzhi98 <55082647+Enzhi98@users.noreply.github.com>
2022-08-22 19:02:54 +0400

    Update index.php

--- resources/views/news/index.php
+++ resources/views/news/index.php
@@ -1,1 +1,8 @@
-<?php
+<?php foreach($newsList as $key => $news):?>
+    <div style="border: 1px solid green">
+        <h2><a href="/news/<?=$key?>"><?=$news['title'] ?></a></h2>
+        <p><?=$news['author'] ?>- <?=$news['created_at']->format('d-m-Y H:i')?></p>
+        <p><?=$news['description'] ?></p>
+        <p><?=$news['category']?></p>
+    </div><br><hr>
+<?php endforeach; ?>
