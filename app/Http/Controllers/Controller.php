2ded6550be3c03a0b55133a0ddce1c856e993682
Enzhi98 <55082647+Enzhi98@users.noreply.github.com>
2022-08-22 18:53:33 +0400

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Faker\Factory;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,54 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function getCategory(int $id_c = null): array
+    {
+        $categories=[];
+        if($id_c) {
+            return   $categories = [
+                '1' => 'Авто',
+                '2' => 'Здоровье',
+                '3' => 'Звезды',
+                '4' => 'Семья',
+            ];
+        }
+        return   $categories = [
+            '1' => 'Авто',
+            '2' => 'Здоровье',
+            '3' => 'Звезды',
+            '4' => 'Семья',
+        ];
+    }
+
+    public function getNews(int $id = null): array
+    {
+        $news = [];
+        $faker = Factory::create();
+        //dd($this->getCategory());
+        $k=array_rand($this->getCategory());
+        if ($id) {
+            return [
+                'title' => $faker->jobTitle(),
+                'author' => $faker->userNAme(),
+                'status' => 'DRAFT',
+                'description' => $faker->text(100),
+                'created_at' => now('Europe/Moscow'),
+                'category' => $this->getCategory()[$k],
+            ];
+
+        }
+        for ($i = 1; $i < 20; $i++) {
+            $k=array_rand($this->getCategory());
+            $news[$i] = [
+                'title' => $faker->jobTitle(),
+                'author' => $faker->userNAme(),
+                'status' => 'DRAFT',
+                'description' => $faker->text(100),
+                'created_at' => now('Europe/Moscow'),
+                'category' => $this->getCategory()[$k],
+            ];
+        }
+        return $news;
+    }
 }
